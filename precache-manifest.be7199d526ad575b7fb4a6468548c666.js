fed00221aa358be9e4dec6d089e3827fccc739b8
Zicozydasilva <Zicozydasilva70@gmail.com>
2020-07-08 12:39:55 +0100

    Updates

---
+++ precache-manifest.be7199d526ad575b7fb4a6468548c666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec7987fae75cfffd5f598e5bdcdc65b",
+    "url": "/QuizbeeApp/index.html"
+  },
+  {
+    "revision": "5e6dc2e2054705b7d7e3",
+    "url": "/QuizbeeApp/static/css/main.ec1f2a1d.chunk.css"
+  },
+  {
+    "revision": "5798c5653883076481f7",
+    "url": "/QuizbeeApp/static/js/2.c9157dd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizbeeApp/static/js/2.c9157dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6dc2e2054705b7d7e3",
+    "url": "/QuizbeeApp/static/js/main.0aef0ba5.chunk.js"
+  },
+  {
+    "revision": "4120f9ea3e0fd67bf9eb",
+    "url": "/QuizbeeApp/static/js/runtime-main.6092eefb.js"
+  }
+]);
